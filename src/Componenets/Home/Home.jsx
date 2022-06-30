2626340d3ab0905846d42bfe29558bf1c86f96e2
Milton D. Yama <miltondyama@outlook.com>
2022-06-29 23:59:10 -0400

    criei um arquivo env e tirei as chaves da aplicacao

--- src/Componenets/Home/Home.jsx
+++ src/Componenets/Home/Home.jsx
@@ -5,16 +5,6 @@ import { BiPlay } from 'react-icons/bi'
 import { AiOutlinePlus } from 'react-icons/ai'
 import './Home.scss'
 
-const apiKey = 'f080850117e6c6c1df95cc119bb5c36b'
-const urlBase = 'https://api.themoviedb.org/3'
-const imageUrl = 'https://image.tmdb.org/t/p/original'
-
-const upcoming = 'upcoming'
-const nowPlaying = 'now_playing'
-const popular = 'popular'
-const topRated = 'top_rated'
-
-
 const Card = ({ image }) => (
   <img className='card' src={image} alt='cover' />
 )
@@ -24,7 +14,7 @@ const Row = ({ title, array = [] }) => (
     <h2>{title}</h2>
     <div>
       {array.map(item => (
-        <Card key={item.id} image={`${imageUrl}/${item.poster_path}`} />
+        <Card key={item.id} image={`${process.env.REACT_APP_IMAGE_URL}/${item.poster_path}`} />
       ))}
     </div>
   </div>
@@ -39,27 +29,27 @@ const Home = () => {
 
   useEffect(() => {
     const fetchUpcoming = async () => {
-      let { data: { results } } = await axios.get(`${urlBase}/movie/${upcoming}?api_key=${apiKey}`)
+      let { data: { results } } = await axios.get(`${process.env.REACT_APP_BASE_URL}/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}`)
       setUpcomingMovies(results)
     }
 
     const fetchNowPlaying = async () => {
-      let { data: { results } } = await axios.get(`${urlBase}/movie/${nowPlaying}?api_key=${apiKey}`)
+      let { data: { results } } = await axios.get(`${process.env.REACT_APP_BASE_URL}/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`)
       setNowPlayingMovies(results)
     }
 
     const fetchPopular = async () => {
-      let { data: { results } } = await axios.get(`${urlBase}/movie/${popular}?api_key=${apiKey}`)
+      let { data: { results } } = await axios.get(`${process.env.REACT_APP_BASE_URL}/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`)
       setPopularMovies(results)
     }
 
     const fetchTopRated = async () => {
-      let { data: { results } } = await axios.get(`${urlBase}/movie/${topRated}?api_key=${apiKey}`)
+      let { data: { results } } = await axios.get(`${process.env.REACT_APP_BASE_URL}/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}`)
       setTopRatedMovies(results)
     }
 
     const fetchGenres = async () => {
-      let { data: { genres } } = await axios.get(`${urlBase}/genre/movie/list?api_key=${apiKey}`)
+      let { data: { genres } } = await axios.get(`${process.env.REACT_APP_BASE_URL}/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`)
       setGenresMovies(genres)
     }
 
@@ -74,7 +64,7 @@ const Home = () => {
     <section className='home'>
       <div className="banner" style={{
         backgroundImage: popularMovies[0]
-          ? `url(${imageUrl}/${popularMovies[0].poster_path})`
+          ? `url(${process.env.REACT_APP_IMAGE_URL}/${popularMovies[0].poster_path})`
           : 'rgb(16, 16, 16)'
       }}>
         { popularMovies[0] && (<h1>{popularMovies[0].original_title}</h1>) }
